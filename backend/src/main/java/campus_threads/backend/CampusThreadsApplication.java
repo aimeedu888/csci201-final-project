db579f4f8b577d0326e14d1e41456c87077a5a79
evanc-ole <80845794+evanc-ole@users.noreply.github.com>
2023-11-06 16:37:39 -0800

    Add files via upload

---
+++ backend/src/main/java/campus_threads/backend/CampusThreadsApplication.java
@@ -0,0 +1,13 @@
+package campus_threads.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusThreadsApplication.class, args);
+	}
+
+}
