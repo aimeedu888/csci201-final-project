bbe66313bd39ae21e1003d737724745f04c48186
Roy Choi <rschoi@usc.edu>
2023-11-25 22:04:26 -0800

    Added HomeController and Index.html (HomeController should make it so the application serves our webpage)

---
+++ src/main/java/campus_threads/backend/api/HomeController.java
@@ -0,0 +1,10 @@
+package campus_threads.backend.api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
